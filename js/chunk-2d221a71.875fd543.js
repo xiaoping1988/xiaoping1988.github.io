852fb82a699a9f266e3ccabdf4b8d508f713cb70
liujiangping <liujiangping@daojia-inc.com>
2019-09-15 11:45:46 +0800

    指标体系、数据API路由

---
+++ js/chunk-2d221a71.875fd543.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d221a71"],{caeb:function(n,u,a){"use strict";a.r(u);var c,e,l=a("2877"),o={},p=Object(l["a"])(o,c,e,!1,null,null,null);u["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d221a71.875fd543.js.map
